a3bdc074b856ddac8dd86a97dbeb98a6f5f16876
lorebitweb <imtheballoon@gmail.com>
2026-02-16 20:41:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lorebot/',
+  base: '/lorebitweb.github.io/',
   css: {
     preprocessorOptions: {
       scss: {
